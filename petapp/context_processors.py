f92a936952be66ca6e831394b31f1b75033e6137
ahmednabeen <ahmednabeen@users.noreply.github.com>
2026-06-08 09:35:35 +0600

    Update footer social icons, add category dropdown nav

--- petapp/context_processors.py
+++ petapp/context_processors.py
@@ -1,4 +1,4 @@
-from .models import SiteSetting, Service, WorkingStep, AboutFeature, WhyChooseUsItem
+from .models import SiteSetting, Service, WorkingStep, AboutFeature, WhyChooseUsItem, Category, Pet
 
 def site_settings(request):
     settings_dict = {}
@@ -9,9 +9,15 @@ def site_settings(request):
     }
 
 def site_data(request):
+    categories = Category.objects.all()
+    nav_categories = []
+    for cat in categories:
+        breeds = list(Pet.objects.filter(category=cat).values_list('species', flat=True).distinct().order_by('species'))
+        nav_categories.append({'name': cat.name, 'breeds': breeds})
     return {
         'all_services': Service.objects.all(),
         'all_working_steps': WorkingStep.objects.all(),
         'all_about_features': AboutFeature.objects.all(),
         'all_why_choose_us': WhyChooseUsItem.objects.all(),
+        'nav_categories': nav_categories,
     }
